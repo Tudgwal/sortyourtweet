1753704daf5fe06dc0b17930db84bfa0871eddbe
tudgwal <gregoire.dauvin@epitech.eu>
2020-01-22 22:20:16 +0200

    modification of the disconnect system

--- disconnect.php
+++ disconnect.php
@@ -2,4 +2,5 @@
 session_start();
 $_SESSION['Twitter_settings']['oauth_access_token'] = null;
 $_SESSION['Twitter_settings']['oauth_access_token_secret'] = null;
+$_SESSION['last_tweet'] = null;
 header('Location: index.php');
